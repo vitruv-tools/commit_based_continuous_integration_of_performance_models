7989d1257f14db104315ab76c0e63d4b2530f1d3
Martin Armbruster <armbrustermartin@googlemail.com>
2021-06-01 15:53:47 +0200

    Renamed the javaim.modelrefinement bundle to javaim with updating the dependencies and Java version.

--- tools.vitruv.applications.javaim.modelrefinement/src/tools/vitruv/applications/javaim/modelrefinement/Java2ImMethodChangeTransformationSpecification.java
+++
@@ -1,9 +0,0 @@
-package tools.vitruv.applications.javaim.modelrefinement;
-
-public class Java2ImMethodChangeTransformationSpecification extends Java2ImMethodBodyChangeTransformation{
-
-	public Java2ImMethodChangeTransformationSpecification() {
-		super();
-	}
-
-}
