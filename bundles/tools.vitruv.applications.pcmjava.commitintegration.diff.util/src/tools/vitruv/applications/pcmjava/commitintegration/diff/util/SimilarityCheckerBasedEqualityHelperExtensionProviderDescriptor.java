7bfac9a99fae25aa7ad8f08aa3012eedb165cbc1
Martin Armbruster <armbrustermartin@googlemail.com>
2021-07-02 11:06:15 +0200

    Added an EqualityHelperExtensionProvider for comparing Java models which delegates the comparison to the SPLevo SimilarityChecker.

---
+++ bundles/tools.vitruv.applications.pcmjava.commitintegration.diff.util/src/tools/vitruv/applications/pcmjava/commitintegration/diff/util/SimilarityCheckerBasedEqualityHelperExtensionProviderDescriptor.java
@@ -0,0 +1,38 @@
+package tools.vitruv.applications.pcmjava.commitintegration.diff.util;
+
+import java.util.regex.Pattern;
+
+import org.eclipse.emf.compare.match.eobject.EqualityHelperExtensionProvider;
+import org.eclipse.emf.compare.utils.IEqualityHelper;
+import org.eclipse.emf.ecore.EObject;
+import org.splevo.jamopp.diffing.similarity.SimilarityChecker;
+
+public class SimilarityCheckerBasedEqualityHelperExtensionProviderDescriptor implements EqualityHelperExtensionProvider.Descriptor {
+	private SimilarityChecker checker;
+
+	public SimilarityCheckerBasedEqualityHelperExtensionProviderDescriptor(
+			SimilarityChecker check) {
+		checker = check;
+	}
+	
+	@Override
+	public EqualityHelperExtensionProvider getEqualityHelperExtensionProvider() {
+		return new EqualityHelperExtensionProvider() {
+			@Override
+			public SpecificMatch matchingEObjects(EObject object1, EObject object2, IEqualityHelper equalityHelper) {
+				Boolean r = checker.isSimilar(object1, object2);
+				return r == null ? SpecificMatch.UNKNOWN : (r == true ? SpecificMatch.MATCH : SpecificMatch.UNMATCH);
+			}
+		};
+	}
+
+	@Override
+	public int getRanking() {
+		return 20;
+	}
+
+	@Override
+	public Pattern getNsURI() {
+		return Pattern.compile(".*");
+	}
+}
