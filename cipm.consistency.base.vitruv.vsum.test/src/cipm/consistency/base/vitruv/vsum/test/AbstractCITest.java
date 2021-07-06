ef0d2af14d5a804333741b7718c4aa5eb9adccd4
Martin Armbruster <armbrustermartin@googlemail.com>
2021-07-06 19:35:59 +0200

    The AbstractCITest cleans the resources after a test case has been performed.

--- cipm.consistency.base.vitruv.vsum.test/src/cipm/consistency/base/vitruv/vsum/test/AbstractCITest.java
+++ cipm.consistency.base.vitruv.vsum.test/src/cipm/consistency/base/vitruv/vsum/test/AbstractCITest.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import cipm.consistency.base.vitruv.vsum.VSUMFacade;
 import tools.vitruv.applications.pcmjava.commitintegration.CommitChangePropagator;
+import tools.vitruv.applications.pcmjava.commitintegration.JavaParserAndPropagatorUtility;
 
 public abstract class AbstractCITest {
+	protected VSUMFacade facade;
 	protected CommitChangePropagator prop;
 	
 	@BeforeEach
@@ -21,10 +24,19 @@ public abstract class AbstractCITest {
 		ap.setTarget(ConsoleAppender.SYSTEM_OUT);
 		logger.addAppender(ap);
 		logger.setLevel(Level.ALL);
-		VSUMFacade fac = new VSUMFacade(Paths.get(getTestPath()));
-		prop = new CommitChangePropagator(new File(getRepositoryPath() + File.separator + ".git")
-				.getAbsoluteFile(), fac.getFileLayout().getJavaPath().toString(), fac.getVSUM());
+		logger = Logger.getLogger(JavaParserAndPropagatorUtility.class.getSimpleName());
+		logger.addAppender(ap);
+		logger.setLevel(Level.ALL);
+		facade = new VSUMFacade(Paths.get(getTestPath()));
+		prop = new CommitChangePropagator(new File(getRepositoryPath())
+				.getAbsoluteFile(), facade.getFileLayout().getJavaPath().toString(), facade.getVSUM());
 		prop.initialize();
+	}
+	
+	@AfterEach
+	public void tearDown() throws Exception {
+		facade.getVSUM().dispose();
+		prop.shutdown();
 	}
 	
 	protected abstract String getTestPath();
