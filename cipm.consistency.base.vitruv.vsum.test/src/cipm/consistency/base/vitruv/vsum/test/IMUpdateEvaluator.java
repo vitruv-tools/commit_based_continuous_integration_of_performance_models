cab1ccae204179e7ca9a9c58099a1228786e968c
Martin Armbruster <armbrustermartin@googlemail.com>
2021-07-16 10:57:13 +0200

    Refactored the IM evaluation to deliver the new result object.

---
+++ cipm.consistency.base.vitruv.vsum.test/src/cipm/consistency/base/vitruv/vsum/test/IMUpdateEvaluator.java
@@ -0,0 +1,106 @@
+package cipm.consistency.base.vitruv.vsum.test;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.palladiosimulator.pcm.repository.BasicComponent;
+import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import org.palladiosimulator.pcm.seff.AbstractAction;
+import org.palladiosimulator.pcm.seff.AbstractLoopAction;
+import org.palladiosimulator.pcm.seff.BranchAction;
+import org.palladiosimulator.pcm.seff.ExternalCallAction;
+import org.palladiosimulator.pcm.seff.InternalAction;
+import org.palladiosimulator.pcm.seff.InternalCallAction;
+import org.palladiosimulator.pcm.seff.ResourceDemandingSEFF;
+import org.palladiosimulator.pcm.seff.ServiceEffectSpecification;
+
+import cipm.consistency.base.models.instrumentation.InstrumentationModel.InstrumentationModel;
+import cipm.consistency.base.models.instrumentation.InstrumentationModel.ServiceInstrumentationPoint;
+import cipm.consistency.base.vitruv.vsum.test.evaluation.IMEvaluationResult;
+
+public class IMUpdateEvaluator {
+	private IMEvaluationResult currentEvalResult;
+	
+	public IMEvaluationResult evaluateIMUpdate(Repository repo, InstrumentationModel im) {
+		currentEvalResult = new IMEvaluationResult();
+		currentEvalResult.unmatchedSEFFElements = new ArrayList<>();
+		
+		for (TreeIterator<EObject> iter = im.eAllContents(); iter.hasNext(); iter.next()) {
+			currentEvalResult.numberAllIP++;
+		}
+		
+		for (RepositoryComponent com : repo.getComponents__Repository()) {
+			if (com instanceof BasicComponent) {
+				BasicComponent basicCom = (BasicComponent) com;
+				for (ServiceEffectSpecification seff :
+						basicCom.getServiceEffectSpecifications__BasicComponent()) {
+					if (seff instanceof ResourceDemandingSEFF) {
+						ResourceDemandingSEFF rdseff = (ResourceDemandingSEFF) seff;
+						var sip = findSIP(im, rdseff);
+						if (sip != null) {
+							currentEvalResult.numberMatchedIP++;
+							checkActionInstrumentationPoints(sip, rdseff);
+						}
+					}
+				}
+			}
+		}
+		return currentEvalResult;
+	}
+	
+	private ServiceInstrumentationPoint findSIP(InstrumentationModel im, ResourceDemandingSEFF seff) {
+		for (var sip : im.getPoints()) {
+			if (sip.getService() == seff) {
+				return sip;
+			}
+		}
+		currentEvalResult.unmatchedSEFFElements.add(seff.getId());
+		return null;
+	}
+	
+	private void checkActionInstrumentationPoints(ServiceInstrumentationPoint sip, ResourceDemandingSEFF seff) {
+		for (AbstractAction aa : seff.getSteps_Behaviour()) {
+			checkActionInstrumentationPoint(sip, aa);
+		}
+	}
+	
+	private void checkActionInstrumentationPoint(ServiceInstrumentationPoint sip, AbstractAction aa) {
+		if (aa instanceof AbstractLoopAction) {
+			AbstractLoopAction loop = (AbstractLoopAction) aa;
+			for (AbstractAction innerAA : loop.getBodyBehaviour_Loop().getSteps_Behaviour()) {
+				checkActionInstrumentationPoint(sip, innerAA);
+			}
+			if (findAIP(sip, loop)) {
+				currentEvalResult.numberMatchedIP++;
+			}
+		} else if (aa instanceof BranchAction) {
+			BranchAction branch = (BranchAction) aa;
+			for (var transition : branch.getBranches_Branch()) {
+				for (AbstractAction innerAA :
+						transition.getBranchBehaviour_BranchTransition().getSteps_Behaviour()) {
+					checkActionInstrumentationPoint(sip, innerAA);
+				}
+			}
+			if (findAIP(sip, branch)) {
+				currentEvalResult.numberMatchedIP++;
+			}
+		} else if (aa instanceof InternalAction || aa instanceof ExternalCallAction
+				|| aa instanceof InternalCallAction) {
+			if (findAIP(sip, aa)) {
+				currentEvalResult.numberMatchedIP++;
+			}
+		}
+	}
+	
+	private boolean findAIP(ServiceInstrumentationPoint sip, AbstractAction aa) {
+		for (var aip : sip.getActionInstrumentationPoints()) {
+			if (aip.getAction() == aa) {
+				return true;
+			}
+		}
+		currentEvalResult.unmatchedSEFFElements.add(aa.getId());
+		return false;
+	}
+}
