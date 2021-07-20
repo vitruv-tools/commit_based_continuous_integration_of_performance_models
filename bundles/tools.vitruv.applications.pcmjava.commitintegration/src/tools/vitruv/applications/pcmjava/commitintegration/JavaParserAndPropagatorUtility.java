2b6d0918b78e399d7e5eb87fb3790d7353d458b6
Martin Armbruster <armbrustermartin@googlemail.com>
2021-07-21 00:49:03 +0200

    Extended the module detection to ask the developer for ambiguous candidates.

--- bundles/tools.vitruv.applications.pcmjava.commitintegration/src/tools/vitruv/applications/pcmjava/commitintegration/JavaParserAndPropagatorUtility.java
+++ bundles/tools.vitruv.applications.pcmjava.commitintegration/src/tools/vitruv/applications/pcmjava/commitintegration/JavaParserAndPropagatorUtility.java
@@ -5,8 +5,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
@@ -20,9 +21,12 @@ import org.emftext.language.java.LogicalJavaURIGenerator;
 import org.emftext.language.java.classifiers.ConcreteClassifier;
 import org.emftext.language.java.containers.CompilationUnit;
 import org.emftext.language.java.containers.JavaRoot;
+import org.emftext.language.java.containers.Origin;
 
 import jamopp.options.ParserOptions;
 import jamopp.parser.jdt.singlefile.JaMoPPJDTSingleFileParser;
+import tools.vitruv.framework.userinteraction.InternalUserInteractor;
+import tools.vitruv.framework.userinteraction.UserInteractionFactory;
 import tools.vitruv.framework.vsum.VirtualModel;
 
 /**
@@ -32,7 +36,8 @@ import tools.vitruv.framework.vsum.VirtualModel;
  */
 public final class JavaParserAndPropagatorUtility {
 	private static final Logger logger = Logger.getLogger(JavaParserAndPropagatorUtility.class.getSimpleName());
-	private static final String POM_FILE_NAME = "pom.xml";
+	private static final String MAVEN_POM_FILE_NAME = "pom.xml";
+	private static final String GRADLE_BUILD_FILE_NAME = "build.gradle";
 	private static final String DOCKERFILE_FILE_NAME = "Dockerfile";
 	
 	private JavaParserAndPropagatorUtility() {
@@ -71,7 +76,7 @@ public final class JavaParserAndPropagatorUtility {
 	}
 	
 	private static void filterResourcesForComponents(ResourceSet resourceSet, Path dir) {
-		HashMap<String, HashSet<Resource>> moduleToCUMap = new HashMap<>();
+		ModuleCandidates candidate = new ModuleCandidates();
 		for (Resource resource : resourceSet.getResources()) {
 			if (resource.getContents().isEmpty()) {
 				continue;
@@ -84,48 +89,95 @@ public final class JavaParserAndPropagatorUtility {
 			} else if (root instanceof CompilationUnit) {
 				if (resource.getURI().isFile()) {
 					Path file = Paths.get(resource.getURI().toFileString()).toAbsolutePath();
-					String potMod = detectModule(file, dir);
-					if (potMod != null) {
-						HashSet<Resource> target;
-						if (moduleToCUMap.containsKey(potMod)) {
-							target = moduleToCUMap.get(potMod);
-						} else {
-							target = new HashSet<>();
-							moduleToCUMap.put(potMod, target);
-						}
-						target.add(resource);
-					}
+					detectModule(resource, file, dir, candidate);
 				}
 			}
 		}
-		moduleToCUMap.entrySet().forEach(entry -> {
-			URI uri = LogicalJavaURIGenerator.getModuleURI(entry.getKey());
+		InternalUserInteractor userInteractor = UserInteractionFactory.instance.createDialogUserInteractor();
+		var modCandidates = new HashMap<>(candidate.getModulesInState(ModuleCandidates.ModuleState.COMPONENT_CANDIDATE));
+		modCandidates.forEach((k, v) -> {
+			int r = userInteractor.getSingleSelectionDialogBuilder().message("Detected the potential component / module"
+				+ k + ". Which type of a component is it?").choices(List.of("Microservice component",
+				"Regular component", "Part of another component", "No component")).startInteraction();
+			if (r == 3) {
+				candidate.removeModule(ModuleCandidates.ModuleState.COMPONENT_CANDIDATE, k);
+			} else {
+				ModuleCandidates.ModuleState newState;
+				if (r == 0) {
+					newState = ModuleCandidates.ModuleState.MICROSERVICE_COMPONENT;
+				} else if (r == 1) {
+					newState = ModuleCandidates.ModuleState.REGULAR_COMPONENT;
+				} else {
+					newState = ModuleCandidates.ModuleState.PART_OF_COMPONENT;
+				}
+				candidate.updateState(ModuleCandidates.ModuleState.COMPONENT_CANDIDATE, newState, k);
+			}
+		});
+		modCandidates = new HashMap<>(candidate.getModulesInState(ModuleCandidates.ModuleState.PART_OF_COMPONENT));
+		modCandidates.forEach((k, v) -> {
+			ArrayList<String> allPossibleModules = new ArrayList<>();
+			allPossibleModules.addAll(candidate.getModulesInState(
+					ModuleCandidates.ModuleState.MICROSERVICE_COMPONENT).keySet());
+			int mscSize = allPossibleModules.size();
+			allPossibleModules.addAll(candidate.getModulesInState(
+					ModuleCandidates.ModuleState.REGULAR_COMPONENT).keySet());
+			int r = userInteractor.getSingleSelectionDialogBuilder().message("The component / module candidate " + k
+					+ " is part of which component / module?").choices(allPossibleModules).startInteraction();
+			String newMod = allPossibleModules.get(r);
+			candidate.removeModule(ModuleCandidates.ModuleState.PART_OF_COMPONENT, k);
+			ModuleCandidates.ModuleState stateToUse;
+			if (r < mscSize) {
+				stateToUse = ModuleCandidates.ModuleState.MICROSERVICE_COMPONENT;
+			} else {
+				stateToUse = ModuleCandidates.ModuleState.REGULAR_COMPONENT;
+			}
+			candidate.getModulesInState(stateToUse).get(newMod).addAll(v);
+		});
+		createModules(candidate.getModulesInState(ModuleCandidates.ModuleState.MICROSERVICE_COMPONENT),
+				resourceSet, Origin.FILE);
+		createModules(candidate.getModulesInState(ModuleCandidates.ModuleState.REGULAR_COMPONENT),
+				resourceSet, Origin.ARCHIVE);
+	}
+	
+	private static void createModules(Map<String, Set<Resource>> map, ResourceSet resourceSet,
+			Origin moduleOrigin) {
+		map.forEach((k, v) -> {
+			URI uri = LogicalJavaURIGenerator.getModuleURI(k);
 			Resource targetResource = resourceSet.getResource(uri, false);
 			if (targetResource == null) {
 				targetResource = resourceSet.createResource(uri);
 			}
 			org.emftext.language.java.containers.Module mod =
 					org.emftext.language.java.containers.ContainersFactory.eINSTANCE.createModule();
-			mod.setName(entry.getKey());
+			mod.setName(k);
+			mod.setOrigin(moduleOrigin);
 			targetResource.getContents().add(mod);
-			entry.getValue().stream().map(resource -> resource.getContents().get(0))
+			v.stream().map(resource -> resource.getContents().get(0))
 				.map(obj -> (CompilationUnit) obj).map(cu -> cu.getChildrenByType(ConcreteClassifier.class))
 				.flatMap(cc -> cc.stream()).map(cc -> cc.getPackage()).filter(p -> p != null)
 				.forEach(p -> p.setModule(mod));
 		});
 	}
 	
-	private static String detectModule(Path file, Path container) {
+	private static void detectModule(Resource res, Path file, Path container, ModuleCandidates candidate) {
 		Path parent = file.getParent();
 		while (container.compareTo(parent) != 0) {
-			boolean fileExistence = checkSiblingExistence(parent, POM_FILE_NAME);
-//					&& checkSiblingExistence(parent, DOCKERFILE_FILE_NAME);
-			if (fileExistence) {
-				return parent.getParent().getFileName().toString();
+			boolean buildFileExistence = checkSiblingExistence(parent, MAVEN_POM_FILE_NAME)
+					|| checkSiblingExistence(parent, GRADLE_BUILD_FILE_NAME);
+			boolean dockerFileExistence = checkSiblingExistence(parent, DOCKERFILE_FILE_NAME);
+			if (buildFileExistence) {
+				String modName = parent.getParent().getFileName().toString();
+				if (dockerFileExistence) {
+					candidate.addModuleClassifier(ModuleCandidates.ModuleState.MICROSERVICE_COMPONENT, 
+							modName, res);
+				} else {
+					candidate.addModuleClassifier(ModuleCandidates.ModuleState.COMPONENT_CANDIDATE,
+							modName, res);
+				}
+				return;
 			}
 			parent = parent.getParent();
 		}
-		return null;
 	}
 	
 	private static boolean checkSiblingExistence(Path file, String siblingName) {
---
+++ bundles/tools.vitruv.applications.pcmjava.commitintegration/src/tools/vitruv/applications/pcmjava/commitintegration/ModuleCandidates.java
@@ -0,0 +1,55 @@
+package tools.vitruv.applications.pcmjava.commitintegration;
+
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+public class ModuleCandidates {
+	public enum ModuleState {
+		MICROSERVICE_COMPONENT,
+		REGULAR_COMPONENT,
+		COMPONENT_CANDIDATE,
+		PART_OF_COMPONENT
+	}
+	
+	private EnumMap<ModuleState, Map<String, Set<Resource>>> candidates;
+	
+	public ModuleCandidates() {
+		candidates = new EnumMap<>(ModuleState.class);
+		for (var value : ModuleState.values()) {
+			candidates.put(value, new HashMap<>());
+		}
+	}
+	
+	public void addModuleClassifier(ModuleState state, String moduleName, Resource cu) {
+		Map<String, Set<Resource>> classMap = getModulesInState(state);
+		Set<Resource> classSet;
+		if (classMap.containsKey(moduleName)) {
+			classSet = classMap.get(moduleName);
+		} else {
+			classSet = new HashSet<>();
+			classMap.put(moduleName, classSet);
+		}
+		classSet.add(cu);
+	}
+	
+	public Map<String, Set<Resource>> getModulesInState(ModuleState state) {
+		return candidates.get(state);
+	}
+	
+	public void updateState(ModuleState oldState, ModuleState newState, String moduleName) {
+		Map<String, Set<Resource>> map = getModulesInState(oldState);
+		Set<Resource> classes = map.remove(moduleName);
+		map = getModulesInState(newState);
+		map.put(moduleName, classes);
+	}
+	
+	public void removeModule(ModuleState state, String moduleName) {
+		Map<String, Set<Resource>> map = getModulesInState(state);
+		map.remove(moduleName);
+	}
+}
