b610cb95e8df306840cdd9d9d73571e1d78be0ab
Martin Armbruster <armbrustermartin@googlemail.com>
2021-08-02 22:35:23 +0200

    If there is an external call without a valid target, the target is created.

--- Vitruv-Applications-PCMJavaAdditionals/cipm.consistency.cpr.javapcm.msa/src/cipm/consistency/cpr/javapcm/internal/InternalUtility.java
+++ Vitruv-Applications-PCMJavaAdditionals/cipm.consistency.cpr.javapcm.msa/src/cipm/consistency/cpr/javapcm/internal/InternalUtility.java
@@ -1,6 +1,12 @@
 package cipm.consistency.cpr.javapcm.internal;
 
 import org.emftext.language.java.classifiers.ConcreteClassifier;
+import org.emftext.language.java.members.Method;
+import org.palladiosimulator.pcm.repository.BasicComponent;
+
+import tools.vitruv.applications.pcmjava.seffstatements.code2seff.extended.CommitIntegrationCodeToSeffFactory;
+import tools.vitruv.applications.pcmjava.seffstatements.pojotransformations.code2seff.FunctionClassificationStrategyForPackageMapping;
+import tools.vitruv.framework.correspondence.CorrespondenceModel;
 
 /**
  * An internal utility class.
@@ -25,5 +31,29 @@ public final class InternalUtility {
 			}
 		}
 		return null;
+	}
+	
+	/**
+	 * Checks if a call to a method is an external call.
+	 * 
+	 * @param method the method to check.
+	 * @param cm the correspondence model.
+	 * @param com basic component in which the call occurs.
+	 * @return true if a call to the method is an external call.
+	 */
+	public static boolean isExternalCall(Method method, CorrespondenceModel cm, BasicComponent com) {
+		var factory = new CommitIntegrationCodeToSeffFactory();
+		class LocalStrategy extends FunctionClassificationStrategyForPackageMapping {
+			public LocalStrategy() {
+				super(factory.createBasicComponentFinding(), cm, com);
+			}
+
+			@Override
+			public boolean isExternalCall(Method call) {
+				return super.isExternalCall(call);
+			}
+		}
+		var strategy = new LocalStrategy();
+		return strategy.isExternalCall(method);
 	}
 }
