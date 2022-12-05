d4670c765d2873303b4fa7701fc07ee91c4bf1c6
Martin Armbruster <armbrustermartin@googlemail.com>
2022-12-05 11:02:41 +0100

    Fully implemented the TEAMMATES test.

--- change-based-adaptive-instrumentation/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/TEAMMATESCITest.java
+++ change-based-adaptive-instrumentation/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/TEAMMATESCITest.java
@@ -12,7 +12,16 @@ import cipm.consistency.cpr.javapcm.teammates.TeammatesJavaPCMChangePropagationS
 import tools.vitruv.framework.propagation.ChangePropagationSpecification;
 
 public class TEAMMATESCITest extends AbstractCITest {
-	private static final String COMMIT_TAG_V_8_10_0 = "fdfdcf1ebcabab50f11b01effa036d4c33ad6783";
+	private static final String COMMIT_TAG_V_8_0_0_RC_0 = "648425746bb9434051647c8266dfab50a8f2d6a3";
+	private static final String[] COMMIT_HASHES = {
+		COMMIT_TAG_V_8_0_0_RC_0,
+		"48b67bae03babf5a5e578aefce47f0285e8de8b4", 
+		"83f518e279807dc7eb7023d008a4d1ab290fefee",
+		"f33d0bcd5843678b832efd8ee2963e72a95ecfc9",
+		"ce4463a8741840fd25a41b14801eab9193c7ed18"
+	};
+	// This version is the next one after the last commit in COMMIT_HASHES.
+	private static final String COMMIT_TAG_V_8_0_0_RC_2 = "8a97db611be37ae1975715723e1913de4fd675e8";
 
 	@BeforeAll
 	public static void setUpForAll() {
@@ -40,11 +49,39 @@ public class TEAMMATESCITest extends AbstractCITest {
 		return new TeammatesJavaPCMChangePropagationSpecification();
 	}
 
-//	@Disabled("Only one test case should run at once.")
+	@Disabled("Only one test case should run at once.")
 	@Test
 	public void testTeammatesIntegration() throws Exception {
-		// Integrates TEAMMATES version 8.10.0.
-		executePropagationAndEvaluation(null, COMMIT_TAG_V_8_10_0, 0);
+		// Integrates TEAMMATES version 8.0.0-rc.0.
+		executePropagationAndEvaluation(null, COMMIT_HASHES[0], 0);
+//		performIndependentEvaluation();
+	}
+	
+	@Disabled("Only one test case should run at once.")
+	@Test
+	public void testTeammatesFirstPropagation() throws Exception {
+		executePropagationAndEvaluation(COMMIT_HASHES[0], COMMIT_HASHES[1], 1);
+//		performIndependentEvaluation();
+	}
+	
+	@Disabled("Only one test case should run at once.")
+	@Test
+	public void testTeammatesSecondPropagation() throws Exception {
+		executePropagationAndEvaluation(COMMIT_HASHES[1], COMMIT_HASHES[2], 1);
+//		performIndependentEvaluation();
+	}
+	
+	@Disabled("Only one test case should run at once.")
+	@Test
+	public void testTeammatesThirdPropagation() throws Exception {
+		executePropagationAndEvaluation(COMMIT_HASHES[2], COMMIT_HASHES[3], 1);
+//		performIndependentEvaluation();
+	}
+	
+	@Disabled("Only one test case should run at once.")
+	@Test
+	public void testTeammatesFourthPropagation() throws Exception {
+		executePropagationAndEvaluation(COMMIT_HASHES[3], COMMIT_HASHES[4], 1);
 //		performIndependentEvaluation();
 	}
 }
