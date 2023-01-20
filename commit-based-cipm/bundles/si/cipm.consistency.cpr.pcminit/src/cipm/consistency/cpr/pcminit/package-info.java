8d1b0a0887f4f45e54f536d21cf69ef4b0e379a7
Lukas Burgey <lukas.burgey@student.kit.edu>
2023-01-20 11:34:54 +0100

    Add comments for a package

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.pcminit/src/cipm/consistency/cpr/pcminit/package-info.java
+++ commit-based-cipm/bundles/si/cipm.consistency.cpr.pcminit/src/cipm/consistency/cpr/pcminit/package-info.java
@@ -1,4 +1,9 @@
 /**
- * Defines the consistency preservation rules between the lua model and the PCM.
+ * This package contains consistency preservation rules that are used to initialize the PCM.
+ * 
+ * Why is this needed?
+ *      Vitruv now differentiates between Correspondences and ReactionCorrespondences.
+ *      Within reactions only the latter seem to be accessible. We need correspondences
+ *      to initialize the PCM and hence also do the initialization in reactions.
  */
 package cipm.consistency.cpr.pcminit;
