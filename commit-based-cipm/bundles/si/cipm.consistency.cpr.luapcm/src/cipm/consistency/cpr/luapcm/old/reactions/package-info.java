53c96020f4cfd9a14e920d73698d88e6c29a5675
Lukas Burgey <lukas.burgey@student.kit.edu>
2023-03-20 18:17:29 +0100

    Remove old CPR structure

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.luapcm/src/cipm/consistency/cpr/luapcm/old/reactions/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Defines reactions for keeping the consistency between a Lua code model and the PCM.
- */
-package cipm.consistency.cpr.luapcm.old.reactions;
--- commit-based-cipm/bundles/si/cipm.consistency.cpr.luapcm/src/cipm/consistency/cpr/luapcm/old/routines/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package defines routines which are called by reactions.
- * 
- * The naming convention for files in this package is:
- *  
- *  "<lua_entity>_<its corresponding pcm entity>"
- */
-package cipm.consistency.cpr.luapcm.old.routines;
