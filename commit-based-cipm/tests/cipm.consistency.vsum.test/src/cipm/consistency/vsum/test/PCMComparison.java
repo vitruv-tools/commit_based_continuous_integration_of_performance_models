d4991f092f163715b5ce9681e1153cbce722643c
Martin Armbruster <armbrustermartin@googlemail.com>
2024-05-27 10:45:15 +0200

    Added a separate class and method to compare PCM Repository models.

---
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/PCMComparison.java
@@ -0,0 +1,34 @@
+package cipm.consistency.vsum.test;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
+import cipm.consistency.commitintegration.diff.util.ComparisonBasedJaccardCoefficientCalculator;
+import cipm.consistency.commitintegration.diff.util.ComparisonBasedJaccardCoefficientCalculator.JaccardCoefficientResult;
+import cipm.consistency.commitintegration.diff.util.pcm.PCMModelComparator;
+
+/**
+ * This class supports the comparison of PCM models by providing appropriate utility methods.
+ * 
+ * @author Martin Armbruster
+ */
+public class PCMComparison {
+	private PCMComparison() {}
+	
+	/**
+	 * Compares two PCM Repository models.
+	 * 
+	 * @param path1 File path to the first PCM Repository model.
+	 * @param path2 File path to the second PCM Repository model.
+	 * @return The result of the Repository model comparison.
+	 */
+	public static JaccardCoefficientResult compareRepositories(String path1, String path2) {
+		ResourceSet set = new ResourceSetImpl();
+		Resource res1 = set.getResource(URI.createFileURI(path1), true);
+		Resource res2 = set.getResource(URI.createFileURI(path2), true);
+		var comp = PCMModelComparator.compareRepositoryModels(res1, res2);
+		return ComparisonBasedJaccardCoefficientCalculator.calculateJaccardCoefficient(comp);
+	}
+}
