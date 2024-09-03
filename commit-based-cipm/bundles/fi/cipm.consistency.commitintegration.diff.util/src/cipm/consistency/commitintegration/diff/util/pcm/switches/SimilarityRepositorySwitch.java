7c79364c929b60cc4b6324a45888cb6572f2cc35
AlpTorac <a.torac@hotmail.com>
2024-09-03 17:52:19 +0200

    (WIP) Adapt inner switches
    Use implemented interfaces in PCM-related inner switches
    
    Use extracted structure in PCM-related inner switches
    
    Non-functional commit

--- commit-based-cipm/bundles/fi/cipm.consistency.commitintegration.diff.util/src/cipm/consistency/commitintegration/diff/util/pcm/switches/SimilarityRepositorySwitch.java
+++ commit-based-cipm/bundles/fi/cipm.consistency.commitintegration.diff.util/src/cipm/consistency/commitintegration/diff/util/pcm/switches/SimilarityRepositorySwitch.java
@@ -13,13 +13,40 @@ import org.palladiosimulator.pcm.repository.Parameter;
 import org.palladiosimulator.pcm.repository.PrimitiveDataType;
 import org.palladiosimulator.pcm.repository.Repository;
 import org.palladiosimulator.pcm.repository.util.RepositorySwitch;
+import org.splevo.jamopp.diffing.similarity.base.ISimilarityRequestHandler;
+import org.splevo.jamopp.diffing.similarity.base.ecore.IComposedSwitchAdapter;
 
-import cipm.consistency.commitintegration.diff.util.pcm.PCMRepositorySimilarityChecker;
+import cipm.consistency.commitintegration.diff.util.pcm.IPCMRepositorySimilaritySwitch;
 
-class SimilarityRepositorySwitch extends RepositorySwitch<Boolean> {
+public class SimilarityRepositorySwitch extends RepositorySwitch<Boolean> implements IPCMPositionInnerSwitch {
+	private IPCMRepositorySimilaritySwitch similaritySwitch;
+	private boolean checkStatementPosition;
+
+	@Override
+	public ISimilarityRequestHandler getSimilarityRequestHandler() {
+		return this.similaritySwitch;
+	}
+
+	@Override
+	public IComposedSwitchAdapter getContainingSwitch() {
+		return this.similaritySwitch;
+	}
+	
+	@Override
+	public boolean shouldCheckStatementPosition() {
+		return this.checkStatementPosition;
+	}
+	
+	public SimilarityRepositorySwitch(
+			IPCMRepositorySimilaritySwitch similaritySwitch,
+			boolean checkStatementPosition) {
+		this.similaritySwitch = similaritySwitch;
+		this.checkStatementPosition = checkStatementPosition;
+	}
+
 	@Override
 	public Boolean caseRepository(Repository repo1) {
-		Repository repo2 = (Repository) compareElement;
+		Repository repo2 = (Repository) this.getCompareElement();
 
 		if (!repo1.getEntityName().equals(repo2.getEntityName())) {
 			return Boolean.FALSE;
@@ -30,26 +57,26 @@ class SimilarityRepositorySwitch extends RepositorySwitch<Boolean> {
 
 	@Override
 	public Boolean caseOperationInterface(OperationInterface opInterface1) {
-		OperationInterface opInterface2 = (OperationInterface) compareElement;
+		OperationInterface opInterface2 = (OperationInterface) this.getCompareElement();
 
 		if (!opInterface1.getEntityName().equals(opInterface2.getEntityName())) {
 			return Boolean.FALSE;
 		}
 
-		return PCMRepositorySimilarityChecker.this.areSimilar(
+		return this.areSimilar(
 				opInterface1.getParentInterfaces__Interface(),
 				opInterface2.getParentInterfaces__Interface());
 	}
 
 	@Override
 	public Boolean caseOperationSignature(OperationSignature sign1) {
-		OperationSignature sign2 = (OperationSignature) compareElement;
+		OperationSignature sign2 = (OperationSignature) this.getCompareElement();
 
 		if (!sign1.getEntityName().equals(sign2.getEntityName())) {
 			return Boolean.FALSE;
 		}
 
-		var result = PCMRepositorySimilarityChecker.this.areSimilar(
+		var result = this.areSimilar(
 				sign1.getParameters__OperationSignature(),
 				sign2.getParameters__OperationSignature());
 
@@ -57,92 +84,92 @@ class SimilarityRepositorySwitch extends RepositorySwitch<Boolean> {
 			return Boolean.FALSE;
 		}
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(sign1.getReturnType__OperationSignature(),
+		return this.isSimilar(sign1.getReturnType__OperationSignature(),
 				sign2.getReturnType__OperationSignature());
 	}
 
 	@Override
 	public Boolean caseParameter(Parameter param1) {
-		Parameter param2 = (Parameter) compareElement;
+		Parameter param2 = (Parameter) this.getCompareElement();
 
 		if (!param1.getParameterName().equals(param2.getParameterName())) {
 			return Boolean.FALSE;
 		}
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(param1.getDataType__Parameter(),
+		return this.isSimilar(param1.getDataType__Parameter(),
 				param2.getDataType__Parameter());
 	}
 
 	@Override
 	public Boolean casePrimitiveDataType(PrimitiveDataType type1) {
-		PrimitiveDataType type2 = (PrimitiveDataType) compareElement;
+		PrimitiveDataType type2 = (PrimitiveDataType) this.getCompareElement();
 		return type1.getType() == type2.getType();
 	}
 
 	@Override
 	public Boolean caseCollectionDataType(CollectionDataType type1) {
-		CollectionDataType type2 = (CollectionDataType) compareElement;
+		CollectionDataType type2 = (CollectionDataType) this.getCompareElement();
 
 		if (!type1.getEntityName().equals(type2.getEntityName())) {
 			return Boolean.FALSE;
 		}
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(type1.getInnerType_CollectionDataType(),
+		return this.isSimilar(type1.getInnerType_CollectionDataType(),
 				type2.getInnerType_CollectionDataType());
 	}
 
 	@Override
 	public Boolean caseCompositeDataType(CompositeDataType type1) {
-		CompositeDataType type2 = (CompositeDataType) compareElement;
+		CompositeDataType type2 = (CompositeDataType) this.getCompareElement();
 
 		if (!type1.getEntityName().equals(type2.getEntityName())) {
 			return Boolean.FALSE;
 		}
 
-		return PCMRepositorySimilarityChecker.this.areSimilar(type1.getParentType_CompositeDataType(),
+		return this.areSimilar(type1.getParentType_CompositeDataType(),
 				type2.getParentType_CompositeDataType());
 	}
 
 	@Override
 	public Boolean caseInnerDeclaration(InnerDeclaration decl1) {
-		InnerDeclaration decl2 = (InnerDeclaration) compareElement;
+		InnerDeclaration decl2 = (InnerDeclaration) this.getCompareElement();
 
 		if (!decl1.getEntityName().equals(decl2.getEntityName())) {
 			return Boolean.FALSE;
 		}
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(decl1.getDatatype_InnerDeclaration(),
+		return this.isSimilar(decl1.getDatatype_InnerDeclaration(),
 				decl2.getDatatype_InnerDeclaration());
 	}
 
 	@Override
 	public Boolean caseBasicComponent(BasicComponent com1) {
-		BasicComponent com2 = (BasicComponent) compareElement;
+		BasicComponent com2 = (BasicComponent) this.getCompareElement();
 
 		return com1.getEntityName().equals(com2.getEntityName());
 	}
 
 	@Override
 	public Boolean caseOperationProvidedRole(OperationProvidedRole opRole1) {
-		OperationProvidedRole opRole2 = (OperationProvidedRole) compareElement;
+		OperationProvidedRole opRole2 = (OperationProvidedRole) this.getCompareElement();
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(
+		return this.isSimilar(
 				opRole1.getProvidedInterface__OperationProvidedRole(),
 				opRole2.getProvidedInterface__OperationProvidedRole());
 	}
 
 	@Override
 	public Boolean caseOperationRequiredRole(OperationRequiredRole reqRole1) {
-		OperationRequiredRole reqRole2 = (OperationRequiredRole) compareElement;
+		OperationRequiredRole reqRole2 = (OperationRequiredRole) this.getCompareElement();
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(
+		return this.isSimilar(
 				reqRole1.getRequiredInterface__OperationRequiredRole(),
 				reqRole2.getRequiredInterface__OperationRequiredRole());
 	}
 
 	@Override
 	public Boolean caseCompositeComponent(CompositeComponent com1) {
-		CompositeComponent com2 = (CompositeComponent) compareElement;
+		CompositeComponent com2 = (CompositeComponent) this.getCompareElement();
 
 		return com1.getEntityName().equals(com2.getEntityName());
 	}
--- commit-based-cipm/bundles/fi/cipm.consistency.commitintegration.diff.util/src/cipm/consistency/commitintegration/diff/util/pcm/switches/SimilaritySeffSwitch.java
+++ commit-based-cipm/bundles/fi/cipm.consistency.commitintegration.diff.util/src/cipm/consistency/commitintegration/diff/util/pcm/switches/SimilaritySeffSwitch.java
@@ -8,10 +8,37 @@ import org.palladiosimulator.pcm.seff.ExternalCallAction;
 import org.palladiosimulator.pcm.seff.ResourceDemandingBehaviour;
 import org.palladiosimulator.pcm.seff.ResourceDemandingSEFF;
 import org.palladiosimulator.pcm.seff.util.SeffSwitch;
+import org.splevo.jamopp.diffing.similarity.base.ISimilarityRequestHandler;
+import org.splevo.jamopp.diffing.similarity.base.ecore.IComposedSwitchAdapter;
 
-import cipm.consistency.commitintegration.diff.util.pcm.PCMRepositorySimilarityChecker;
+import cipm.consistency.commitintegration.diff.util.pcm.IPCMRepositorySimilaritySwitch;
 
-class SimilaritySeffSwitch extends SeffSwitch<Boolean> {
+public class SimilaritySeffSwitch extends SeffSwitch<Boolean> implements IPCMPositionInnerSwitch {
+	private IPCMRepositorySimilaritySwitch similaritySwitch;
+	private boolean checkStatementPosition;
+
+	@Override
+	public ISimilarityRequestHandler getSimilarityRequestHandler() {
+		return this.similaritySwitch;
+	}
+
+	@Override
+	public IComposedSwitchAdapter getContainingSwitch() {
+		return this.similaritySwitch;
+	}
+	
+	@Override
+	public boolean shouldCheckStatementPosition() {
+		return this.checkStatementPosition;
+	}
+	
+	public SimilaritySeffSwitch(
+			IPCMRepositorySimilaritySwitch similaritySwitch,
+			boolean checkStatementPosition) {
+		this.similaritySwitch = similaritySwitch;
+		this.checkStatementPosition = checkStatementPosition;
+	}
+
 	private Boolean checkPositionInContainer(AbstractAction action1, AbstractAction action2) {
 		ResourceDemandingBehaviour parent1 = (ResourceDemandingBehaviour) action1.eContainer();
 		ResourceDemandingBehaviour parent2 = (ResourceDemandingBehaviour) action2.eContainer();
@@ -22,29 +49,29 @@ class SimilaritySeffSwitch extends SeffSwitch<Boolean> {
 
 	@Override
 	public Boolean caseAbstractAction(AbstractAction action1) {
-		AbstractAction action2 = (AbstractAction) compareElement;
+		AbstractAction action2 = (AbstractAction) getCompareElement();
 		return checkPositionInContainer(action1, action2);
 	}
 
 	@Override
 	public Boolean caseResourceDemandingBehaviour(ResourceDemandingBehaviour behav1) {
-		ResourceDemandingBehaviour behav2 = (ResourceDemandingBehaviour) compareElement;
-		return PCMRepositorySimilarityChecker.this.areSimilar(behav1.getSteps_Behaviour(), behav2.getSteps_Behaviour());
+		ResourceDemandingBehaviour behav2 = (ResourceDemandingBehaviour) getCompareElement();
+		return areSimilar(behav1.getSteps_Behaviour(), behav2.getSteps_Behaviour());
 	}
 
 	@Override
 	public Boolean caseResourceDemandingSEFF(ResourceDemandingSEFF seff1) {
-		ResourceDemandingSEFF seff2 = (ResourceDemandingSEFF) compareElement;
+		ResourceDemandingSEFF seff2 = (ResourceDemandingSEFF) getCompareElement();
 
-		return PCMRepositorySimilarityChecker.this.isSimilar(seff1.getDescribedService__SEFF(),
+		return isSimilar(seff1.getDescribedService__SEFF(),
 				seff2.getDescribedService__SEFF());
 	}
 
 	@Override
 	public Boolean caseCollectionIteratorAction(CollectionIteratorAction action1) {
-		CollectionIteratorAction action2 = (CollectionIteratorAction) compareElement;
+		CollectionIteratorAction action2 = (CollectionIteratorAction) getCompareElement();
 
-		var result = PCMRepositorySimilarityChecker.this.isSimilar(
+		var result = isSimilar(
 				action1.getParameter_CollectionIteratorAction(),
 				action2.getParameter_CollectionIteratorAction());
 
@@ -57,7 +84,7 @@ class SimilaritySeffSwitch extends SeffSwitch<Boolean> {
 
 	@Override
 	public Boolean caseAbstractBranchTransition(AbstractBranchTransition transition1) {
-		AbstractBranchTransition transition2 = (AbstractBranchTransition) compareElement;
+		AbstractBranchTransition transition2 = (AbstractBranchTransition) getCompareElement();
 
 		BranchAction parent1 = (BranchAction) transition1.eContainer();
 		BranchAction parent2 = (BranchAction) transition2.eContainer();
@@ -68,9 +95,9 @@ class SimilaritySeffSwitch extends SeffSwitch<Boolean> {
 
 	@Override
 	public Boolean caseExternalCallAction(ExternalCallAction action1) {
-		ExternalCallAction action2 = (ExternalCallAction) compareElement;
+		ExternalCallAction action2 = (ExternalCallAction) getCompareElement();
 
-		var result = PCMRepositorySimilarityChecker.this.isSimilar(
+		var result = isSimilar(
 				action1.getCalledService_ExternalService(),
 				action2.getCalledService_ExternalService());
 
