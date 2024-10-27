fcfb1531287afe40b8c61c46dcb469ed9d6b1d68
AlpTorac <a.torac@hotmail.com>
2024-10-27 19:09:22 +0100

    Add package commentary

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/params/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Contains various utility classes/interfaces that can be used in tests that
+ * use initialisers. This includes elements that provide access to and adapt
+ * initialiser instances
+ * ({@link cipm.consistency.fitests.similarity.params.IInitialiserParameters})
+ * and elements that help bundle test results
+ * ({@link cipm.consistency.fitests.similarity.params.ISimilarityValues}).
+ * <br>
+ * <br>
+ * The singleton
+ * {@link cipm.consistency.fitests.similarity.params.InitialiserTestSettingsProvider}
+ * can be used to configure the elements mentioned above for all tests.
+ */
+package cipm.consistency.fitests.similarity.params;
