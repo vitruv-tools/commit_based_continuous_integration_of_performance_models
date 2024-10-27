60195376fcbd821611f169c48c9b324da61a6819
AlpTorac <a.torac@hotmail.com>
2024-10-27 19:59:11 +0100

    Add advanced tests for JaMoPP-related similarity checking
    These tests aim to verify similarity checking of JaMoPP objects, whose
    construction is not as simple as those from "impltests" and
    "interfacetests".
    
    The cases addressed in these tests were separated from "impltests",
    because they each require a complicated setup, in order to cover certain
    branches of the similarity checking logic.
    
    IdentifierReferenceContainerTest is disabled till NPEs are fixed.

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/complextests/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Contains unit tests for {@link EObject} classes/interfaces within JaMoPP. In
+ * the said tests, {@link EObject} instances are constructed programmatically
+ * and are checked for similarity. <br>
+ * <br>
+ * The tests within this package are meant to test more advanced cases, which
+ * are not covered in
+ * {@link cipm.consistency.fitests.similarity.jamopp.unittests.impltests} or
+ * {@link cipm.consistency.fitests.similarity.jamopp.unittests.interfacetests}.
+ * Such cases involve multiple attributes of 2 {@link EObject} instances being
+ * different and/or more advanced test setup.
+ * 
+ * TODO Add complex tests for AdditionalField (once the missing case methods are
+ * added)
+ * 
+ * TODO Add complex tests for LocalVariableStatement with container Variable
+ * 
+ * TODO Look for further complex test cases (once the missing case methods are
+ * added)
+ */
+package cipm.consistency.fitests.similarity.jamopp.unittests.complextests;
