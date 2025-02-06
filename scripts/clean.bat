88f3b6bac97a1c3bab160fcc8f851bc9d5b6b635
AlpTorac <a.torac@hotmail.com>
2025-02-06 22:07:55 +0100

    Fix clean.bat

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -2,9 +2,6 @@
 cd Vitruv
 git stash --include-untracked
 cd ..
-cd Palladio-ReverseEngineering-SoMoX-JaMoPP
-git stash --include-untracked
-cd ..
 git submodule deinit -f --all
 
 :: Clean all ignored files.
